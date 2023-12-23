1e4946e502c2053d8f9448ef8c6384207c19ef13
Dipankar Adhikary <dipu98adhikary@gmail.com>
2023-12-23 11:57:16 +0530

    Live App 1.3o

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,17 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  title = 'MY';
-  document.addEventListener('DOMContentLoaded', function () {
-  const menuIcon: HTMLElement | null = document.getElementById('menu-icon');
-  const navLinks: HTMLElement | null = document.getElementById('nav-links');
 
-  if (menuIcon && navLinks) {
-    menuIcon.addEventListener('click', function () {
-      navLinks.style.display = (navLinks.style.display === 'none' || navLinks.style.display === '') ? 'block' : 'none';
-    });
-  }
-});
 
 }
